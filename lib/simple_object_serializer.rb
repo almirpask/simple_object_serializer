b4080fa0f833b14e6db711bb6074399950545535
almirpask <almirpask@hotmail.com>
2019-05-26 19:33:10 -0300

    work with params

--- lib/simple_object_serializer.rb
+++ lib/simple_object_serializer.rb
@@ -1,7 +1,16 @@
 module SimpleObjectSerializer
   class Serializer
-    def objects
-      puts 'setup'
+    def objects(*args)
+      parameters = args.last
+      objects = args.first
+      new_object = {}
+      parameters.each do |parameter_index, parameter|
+        parameter.each do |key|
+          new_object[parameter_index] = {} if new_object[parameter_index].nil?
+          new_object[parameter_index][key] = objects[parameter_index][key]
+        end
+      end
+      new_object
     end
   end
 end
